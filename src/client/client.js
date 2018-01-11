25d313407ec0c63506c1384b6f8bba0eab028d8e
Nicholas Johnston <njohnst@github.com>
2018-01-10 19:33:23 -0800

    Refactor client to allow removal of listeners after ICE negotiation

--- src/client/client.js
+++ src/client/client.js
@@ -1,33 +1,45 @@
 const SimplePeer = require('simple-peer')
 const Primus = require('./primusClient.js')
 
+function sendSignal (data) {
+  this.primus.write(data)
+}
+
+function setSignal (data) {
+  if (data.type === 'answer' || data.candidate) {
+    this.rtc.signal(data)
+  }
+}
+
+function addPrimusSignaling (client) {
+  client.primus.on('data', setSignal, client)
+}
+
+function removePrimusSignaling () {
+  this.primus.removeListener('data')
+}
+
 function MeltedClient (url, primusConfig, simplePeerConfig) {
+  if (url.match(/[\w]+:\/\/.+:\d{1,5}/)) this.url = url
+  else throw new Error(`URL ${url} doesn't follow pattern PROTOCOL://HOST:PORT`)
+
+  this.primusConfig = primusConfig
+    simplePeerConfig.initiator = true //NOTE Server is peer, we initiate
+    if (!simplePeerConfig.config) {
+      simplePeerConfig.config = { iceServers: [] }
+    }
+  this.simplePeerConfig = simplePeerConfig
+
     this.establishDataChannel = function () {
       if (SimplePeer.WEBRTC_SUPPORT) {
-        simplePeerConfig.initiator = true //NOTE Server is peer, we initiate
-        if (!simplePeerConfig.config) {
-          simplePeerConfig.config = { iceServers: [] }
-        }
-        const rtc = new SimplePeer(simplePeerConfig)
+        this.rtc = new SimplePeer(this.simplePeerConfig)
+        this.rtc.on('signal', sendSignal.bind(this))
 
-        rtc.on('signal', (data) => {
-          //FIXME - how to determine what the message is on server-side?
-          this.primus.write(data)
-        })
-
-        this.primus.on('data', (data) => {
-          //TODO
-          if (data.type === 'answer' || data.candidate) {
-            rtc.signal(data)
-          }
-        })
-
-        rtc.on('connect', () => {
-          console.log(`RTC connected`)
-          this.primus.removeListener('data')
-        })
-
-        this.rtc = rtc
+        //Add listeners for RTC signaling, and remove them once connected
+        addPrimusSignaling(this)
+        this.rtc.on('connect', removePrimusSignaling.bind(this))
+      } else {
+        throw new Error(`SimplePeer.WEBRTC_SUPPORT evaluated to false`)
       }
     }
 
--- src/server/server.js
+++ src/server/server.js
@@ -11,46 +11,49 @@ function Melted (ip, wsPort, rtcPort, primusConfig, simplePeerConfig) {
   this.rtcPort = rtcPort
   this.primusConfig = primusConfig
   this.simplePeerConfig = simplePeerConfig
+  const transformer = require('../ice-transformer')(this.ip, this.rtcPort)
+
+  this.simplePeerConfig.initiator = false
+  if (!this.simplePeerConfig.config) {
+    //Sanity check - if no ICE servers are passed, pass an empty array
+    this.simplePeerConfig.config = { iceServers: [] }
+  }
+  this.simplePeerConfig.wrtc = wrtc
+  this.simplePeerConfig.sdpTransform = transformer.sdp
 
   this.start = function () {
-    const transformer = require('../ice-transformer')(this.ip, this.rtcPort)
     const primus = require('./primus-loader')(server, this.primusConfig)
 
     server.listen(this.wsPort)
     //NOTE Client is peer, they are initiator
-    this.simplePeerConfig.initiator = false
-    if (!this.simplePeerConfig.config) {
-      this.simplePeerConfig.config = { iceServers: [] }
-    }
-    this.simplePeerConfig.wrtc = wrtc
-    this.simplePeerConfig.sdpTransform = transformer.sdp
+
+//XXX
+    primus.on('connection', this.peerConnect, this)
+
+  }
+
+  this.peerConnect = function (spark) {
+    //TODO
     const rtc = new SimplePeer(this.simplePeerConfig)
 
-    primus.on('connection', function (spark) {
+    spark.on('data', function (data) {
+      if (data.type === 'offer' || data.candidate) {
+        rtc.signal(data)
+        console.log(data)
+      }
+    })
 
-      spark.on('data', function (data) {
-        if (data.candidate || data.type === 'offer') {
-          rtc.signal(data)
-        }
-      })
-
-      rtc.on('signal', function (data) {
-        if (data.type === 'answer') {
-          data.sdp = transformer.candidate(transformer.sdp(data.sdp))
-          spark.write(data)
-        } else if (data.candidate) {
-          data.candidate.candidate = transformer.candidate(data.candidate.candidate)
-          console.log(data)
-          spark.write(data)
-        } else {
-          spark.write(data)
-        }
-      })
-
-      rtc.on('connect', function () {
-        rtc.write('Hello world!')
-      })
-
+    rtc.on('signal', function (data) {
+      if (data.type === 'answer') {
+        data.sdp = transformer.candidate(transformer.sdp(data.sdp))
+        spark.write(data)
+      } else if (data.candidate) {
+        data.candidate.candidate = transformer.candidate(data.candidate.candidate)
+        //console.log(data)
+        spark.write(data)
+      } else {
+        spark.write(data)
+      }
     })
   }
 }
