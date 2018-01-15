7271933968b7b137cc28c85776205bcca8d31e36
Nicholas Johnston <njohnst@github.com>
2018-01-14 18:40:09 -0800

    Refactor ICE transformer to use one function for replacements & add IPV6

--- src/ice-transformer.js
+++ src/ice-transformer.js
@@ -1,35 +1,31 @@
 const Transformer = function (ip, port) {
-  this._candidateReplace = function (c) {
-      return c.replace(
-               /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) (\d{1,5})/,
-               `${ip} ${port}`
-             )
-  }
-
-  this._sdpReplace = function (o) {
-    return this._candidateReplace(
-             o.replace(/m=application\s([^\s]+)\s([^\s]+)\s([^\s]+)/,
-                       `m=application $1 $2 ${port}`)
-              .replace(
-                /a=sctpmap:([^\s]+)\s([^\s]+)\s([^\s]+)/,
-                `a=sctpmap:${port} $2 $3`
-              )
-           )
-
+  this._replace = function (o) {
+    return o.replace(
+       /m=application\s([^\s]+)\s([^\s]+)\s([^\s]+)/,
+       `m=application $1 $2 ${port}`
+      )
+      .replace(
+       /a=sctpmap:([^\s]+)\s([^\s]+)\s([^\s]+)/,
+       `a=sctpmap:${port} $2 $3`
+      )
+      .replace(
+       /a=candidate:([^\s]+)\s([^\s]+)\s([^\s]+)\s([^\s]+)\s([^\s]+)\s([^\s]+)/,
+       `a=candidate:$1 $2 $3 $4 ${ip} ${port}`
+      )
   }
 
   this.candidate = function (c) {
-    c.candidate.candidate = this._candidateReplace(c.candidate.candidate)
+    c.candidate.candidate = this._replace(c.candidate.candidate)
     return c
   }
 
   this.offer = function (o) {
-    o.sdp = this._sdpReplace(o.sdp)
+    o.sdp = this._replace(o.sdp)
     return o
   }
 
   this.answer = function (a) {
-    a.sdp = this._sdpReplace(a.sdp)
+    a.sdp = this._replace(a.sdp)
     return a
   }
 }
