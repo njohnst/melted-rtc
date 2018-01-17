18e18186489a27becdc5b1cb387fc886bafc8176
Nicholas Johnston <njohnst@github.com>
2018-01-16 22:05:58 -0800

    Refactor client & server to use Module pattern (IIFE)

--- src/client/client.js
+++ src/client/client.js
@@ -1,49 +1,52 @@
-const SimplePeer = require('simple-peer')
-const Primus = require('./primusClient.js')
+module.exports = (function () {
+  const SimplePeer = require('simple-peer')
+  const Primus = require('./primusClient.js')
 
-function sendSignal (data) {
-  this.primus.write(data)
-}
-
-function setSignal (data) {
-  if (data.type === 'answer' || data.candidate) {
-    this.rtc.signal(data)
+  function sendSignal (data) {
+    this.primus.write(data)
   }
-}
 
-function addPrimusSignaling (client) {
-  client.primus.on('data', setSignal, client)
-}
-
-function removePrimusSignaling () {
-  this.primus.removeListener('data')
-}
-
-function MeltedClient (url, primusConfig, simplePeerConfig) {
-  if (url.match(/[\w]+:\/\/.+:\d{1,5}/)) this.url = url
-  else throw new Error(`URL ${url} doesn't follow pattern PROTOCOL://HOST:PORT`)
-
-  this.primusConfig = primusConfig
-    simplePeerConfig.initiator = true //NOTE Server is peer, we initiate
-    if (!simplePeerConfig.config) {
-      simplePeerConfig.config = { iceServers: [] }
+  function setSignal (data) {
+    if (data.type === 'answer' || data.candidate) {
+      this.rtc.signal(data)
     }
-  this.simplePeerConfig = simplePeerConfig
+  }
 
-  this.establishDataChannel = function () {
-    if (SimplePeer.WEBRTC_SUPPORT) {
-      this.rtc = new SimplePeer(this.simplePeerConfig)
-      this.rtc.on('signal', sendSignal.bind(this))
+  function addPrimusSignaling (client) {
+    client.primus.on('data', setSignal, client)
+  }
 
-      //Add listeners for RTC signaling, and remove them once connected
-      addPrimusSignaling(this)
-      this.rtc.on('connect', removePrimusSignaling.bind(this))
+  function removePrimusSignaling () {
+    this.primus.removeListener('data')
+  }
+
+  return function (url, primusConfig, simplePeerConfig) {
+    if (url.match(/[\w]+:\/\/.+:\d{1,5}/)) {
+      this.url = url
     } else {
-      throw new Error(`SimplePeer.WEBRTC_SUPPORT evaluated to false`)
+      throw new Error(`URL ${url} doesn't follow pattern PROTOCOL://HOST:PORT`)
     }
+
+    this.primusConfig = primusConfig
+      simplePeerConfig.initiator = true //NOTE Server is peer, we initiate
+      if (!simplePeerConfig.config) {
+        simplePeerConfig.config = { iceServers: [] }
+      }
+    this.simplePeerConfig = simplePeerConfig
+
+    this.establishDataChannel = function () {
+      if (SimplePeer.WEBRTC_SUPPORT) {
+        this.rtc = new SimplePeer(this.simplePeerConfig)
+        this.rtc.on('signal', sendSignal.bind(this))
+
+        //Add listeners for RTC signaling, and remove them once connected
+        addPrimusSignaling(this)
+        this.rtc.on('connect', removePrimusSignaling.bind(this))
+      } else {
+        throw new Error(`SimplePeer.WEBRTC_SUPPORT evaluated to false`)
+      }
+    }
+
+    this.primus = new Primus(url, primusConfig)
   }
-
-  this.primus = new Primus(url, primusConfig)
-}
-
-module.exports = MeltedClient
+})()
--- src/server/server.js
+++ src/server/server.js
@@ -1,78 +1,78 @@
-const express = require('express')
-const app = express()
-const server = require('http').createServer(app)
+module.exports = (function () {
+  const express = require('express')
+  const app = express()
+  const server = require('http').createServer(app)
 
-const SimplePeer = require('simple-peer')
-const wrtc = require('wrtc')
+  const SimplePeer = require('simple-peer')
+  const wrtc = require('wrtc')
 
-function Melted (ip, wsPort, rtcPort, primusConfig, simplePeerConfig) {
-  if (!ip || !wsPort || !rtcPort) {
-    throw new Error('Invalid arguments: IP, WS and RTC ports must be provided')
-  }
-  this.ip = ip
-  this.wsPort = wsPort
-  this.rtcPort = rtcPort
-  this.primusConfig = primusConfig || {}
-  this.simplePeerConfig = simplePeerConfig || {}
   const Transformer = require('../ice-transformer')
-  transformer = new Transformer(this.ip, this.rtcPort)
 
+  return function (ip, wsPort, rtcPort, primusConfig, simplePeerConfig) {
+    if (!ip || !wsPort || !rtcPort) {
+      throw new Error('Invalid arguments: IP, WS and RTC ports must be provided')
+    }
+    this.ip = ip
+    this.wsPort = wsPort
+    this.rtcPort = rtcPort
+    this.primusConfig = primusConfig || {}
+    this.simplePeerConfig = simplePeerConfig || {}
+    transformer = new Transformer(this.ip, this.rtcPort)
 
-  this.simplePeerConfig.initiator = false
-  if (!this.simplePeerConfig.config) {
-    //Sanity check - if no config is passed, pass an empty array for iceServers
-    this.simplePeerConfig.config = { iceServers: [] }
+    this.simplePeerConfig.initiator = false
+    if (!this.simplePeerConfig.config) {
+      //Sanity check - if no config is passed, pass an empty array for iceServers
+      this.simplePeerConfig.config = { iceServers: [] }
+    }
+    this.simplePeerConfig.wrtc = wrtc
+    if (typeof this.simplePeerConfig.trickle === 'undefined') {
+      //Unless otherwise specified, disable trickle
+      this.simplePeerConfig.trickle = false
+    }
+    this.simplePeerConfig.sdpTransform = transformer._sdp
+
+    this.start = function () {
+      const primus = require('./primus-loader')(server, this.primusConfig)
+
+      server.listen(this.wsPort)
+      //NOTE Client is peer, they are initiator
+
+  //XXX
+      primus.on('connection', this.peerConnect, this)
+
+    }
+
+    this.peerConnect = function (spark) {
+      //TODO
+      const rtc = new SimplePeer(this.simplePeerConfig)
+
+      spark.on('data', function (data) {
+        if (data.type === 'offer' || data.candidate) {
+          console.log('rem')
+          console.log(data)
+          rtc.signal(data)
+        }
+      })
+
+      rtc.on('signal', function (data) {
+        if (data.type === 'answer') {
+          console.log('locBefore')
+          console.log(data)
+          data = transformer.answer(data)
+          console.log('locAfter')
+          console.log(data)
+          spark.write(data)
+        } else if (data.candidate) {
+          console.log('locBefore')
+          console.log(data)
+          data = transformer.candidate(data)
+          console.log('locAfter')
+          console.log(data)
+          spark.write(data)
+        } else {
+          spark.write(data)
+        }
+      })
+    }
   }
-  this.simplePeerConfig.wrtc = wrtc
-  if (typeof this.simplePeerConfig.trickle === 'undefined') {
-    //Unless otherwise specified, disable trickle
-    this.simplePeerConfig.trickle = false
-  }
-  this.simplePeerConfig.sdpTransform = transformer._sdp
-
-  this.start = function () {
-    const primus = require('./primus-loader')(server, this.primusConfig)
-
-    server.listen(this.wsPort)
-    //NOTE Client is peer, they are initiator
-
-//XXX
-    primus.on('connection', this.peerConnect, this)
-
-  }
-
-  this.peerConnect = function (spark) {
-    //TODO
-    const rtc = new SimplePeer(this.simplePeerConfig)
-
-    spark.on('data', function (data) {
-      if (data.type === 'offer' || data.candidate) {
-        console.log('rem')
-        console.log(data)
-        rtc.signal(data)
-      }
-    })
-
-    rtc.on('signal', function (data) {
-      if (data.type === 'answer') {
-        console.log('locBefore')
-        console.log(data)
-        data = transformer.answer(data)
-        console.log('locAfter')
-        console.log(data)
-        spark.write(data)
-      } else if (data.candidate) {
-        console.log('locBefore')
-        console.log(data)
-        data = transformer.candidate(data)
-        console.log('locAfter')
-        console.log(data)
-        spark.write(data)
-      } else {
-        spark.write(data)
-      }
-    })
-  }
-}
-
-module.exports = Melted
+})()
